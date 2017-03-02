3d5919804b5058146eb6cf02d5c9a56a97ccdb68
Johsta88 <axces@hotmail.de>
2017-03-02 21:53:45 +0100

    Chat-App BasicVersion

---
+++ app/src/main/java/com/example/johsta/other/WsConfig.java
@@ -0,0 +1,9 @@
+package com.example.johsta.other;
+
+/**
+ * Created by Johsta on 02.03.2017.
+ */
+
+public class WsConfig {
+    public static final String URL_WEBSOCKET = "ws://192.168.0.185:81";
+}
